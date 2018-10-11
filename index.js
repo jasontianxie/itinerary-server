6404d471eeac432246c212282bb78c4a4a928fea
W10-01706171740\Administrator <123@123.com>
2018-10-12 00:12:28 +0800

    return mainPageSlideData.json

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const mainPageSlideData =[
+    {pic:'https://pic.qyer.com/public/home/focus/2018/10/04/15386419503307?imageMogr2/interlace/1|imageslim',href:'#',description:'this is a test'},
+    {pic:'https://pic.qyer.com/public/home/focus/2018/10/04/15386419503307?imageMogr2/interlace/1|imageslim',href:'#',description:'this is a test'},
+    {pic:'https://pic.qyer.com/public/home/focus/2018/10/04/15386419503307?imageMogr2/interlace/1|imageslim',href:'#',description:'this is a test'},
+    {pic:'https://pic.qyer.com/public/home/focus/2018/10/04/15386419503307?imageMogr2/interlace/1|imageslim',href:'#',description:'this is a test'},
+    {pic:'https://pic.qyer.com/public/home/focus/2018/10/04/15386419503307?imageMogr2/interlace/1|imageslim',href:'#',description:'this is a test'}
+]
+
+app.get('/mainPageSlideData.json', (req, res) => {console.log('request is comming');res.send(mainPageSlideData)})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
