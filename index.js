3885b7d9ef85a24d9943d860e39656865ce15094
jason <1986tianxie@sina.com>
2018-10-12 12:11:59 +0800

    add hyper url link

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 const port = 8000
 
 const mainPageSlideData =[
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15380434541828.jpeg',href:'#',description:'this is a test'},
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15381044615697.jpeg',href:'#',description:'this is a test'},
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15381045921996.jpeg',href:'#',description:'this is a test'},
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15381046688621.jpeg',href:'#',description:'this is a test'},
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15381048978867.jpeg',href:'#',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15380434541828.jpeg',href:'/#/second',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15381044615697.jpeg',href:'/#/second',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15381045921996.jpeg',href:'/#/second',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15381046688621.jpeg',href:'/#/second',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15381048978867.jpeg',href:'/#/second',description:'this is a test'},
 ]
 
 app.use('/public',express.static('static'));
