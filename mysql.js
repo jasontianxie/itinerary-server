70aab61355f9dc44cc83fabbd1b4ca06df808182
W10-01706171740\Administrator <123@123.com>
2018-10-17 00:15:26 +0800

    connect mysql

--- mysql.js
+++ mysql.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 8000;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '123456ABCabc',
+    database:'itinerary'
+});
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  
+    console.log('connected as id ' + connection.threadId);
+  });
+
 
 const mainPageSlideData =[
     {pic:'http://localhost:8000/public/mainPageSlidePics/15380434541828.jpeg',href:'/#/second',description:'this is a test'},
@@ -10,12 +28,6 @@ const mainPageSlideData =[
     {pic:'http://localhost:8000/public/mainPageSlidePics/15381048978867.jpeg',href:'/#/second',description:'this is a test'},
 ]
 
-const mainPageSpotsData =[
-    'chongqing',
-    'sichuan',
-    'shanxi',
-    'zhejiang'
-]
 
 app.use('/public',express.static('static'));
 
@@ -28,7 +40,11 @@ app.get('/mainPageSlideData.json', (req, res) => {
 app.get('/mainPageSpotsData.json', (req, res) => {
     console.log('mainPageSpotsData request is comming '+new Date());
     res.header('Access-Control-Allow-Origin', '*');
-    res.send(mainPageSpotsData.map((item)=>item+Math.floor(10*Math.random())));
+    connection.query("SELECT distinct startspotname from itineraries WHERE startspotname LIKE '%"+req.query.search+"%'", function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send(results.map((item)=>item.startspotname));
+      });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
