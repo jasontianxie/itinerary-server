db032d90468cda2fc0813753afb566cceee775bb
jason <1986tianxie@sina.com>
2018-10-16 18:58:19 +0800

    return autocomplete data

--- index.js
+++ index.js
@@ -10,12 +10,25 @@ const mainPageSlideData =[
     {pic:'http://localhost:8000/public/mainPageSlidePics/15381048978867.jpeg',href:'/#/second',description:'this is a test'},
 ]
 
+const mainPageSpotsData =[
+    'chongqing',
+    'sichuan',
+    'shanxi',
+    'zhejiang'
+]
+
 app.use('/public',express.static('static'));
 
 app.get('/mainPageSlideData.json', (req, res) => {
-    console.log('request is comming');
+    console.log('mainPageSlideData request is comming '+new Date());
     res.header('Access-Control-Allow-Origin', '*');
-    res.send(mainPageSlideData)
+    res.send(mainPageSlideData);
+})
+
+app.get('/mainPageSpotsData.json', (req, res) => {
+    console.log('mainPageSpotsData request is comming '+new Date());
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send(mainPageSpotsData.map((item)=>item+Math.floor(10*Math.random())));
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
