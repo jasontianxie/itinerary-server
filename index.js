7c2fdbfb4789711efb4195914fee92d845d735ff
jason <1986tianxie@sina.com>
2018-12-24 14:14:35 +0800

    跳转到写游记界面

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const djkstra = require('./algorithm/djikstra');
 const bodyParser = require('body-parser');
 
 const mainPageSlideData =[
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15380434541828.jpeg',href:'/#/second',description:'this is a test'},
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15381044615697.jpeg',href:'/#/second',description:'this is a test'},
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15381045921996.jpeg',href:'/#/second',description:'this is a test'},
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15381046688621.jpeg',href:'/#/second',description:'this is a test'},
-    {pic:'http://localhost:8000/public/mainPageSlidePics/15381048978867.jpeg',href:'/#/second',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15380434541828.jpeg',href:'#',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15381044615697.jpeg',href:'#',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15381045921996.jpeg',href:'#',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15381046688621.jpeg',href:'#',description:'this is a test'},
+    {pic:'http://localhost:8000/public/mainPageSlidePics/15381048978867.jpeg',href:'#',description:'this is a test'},
 ]
 
 const mainPageSpotsData =[
--- modles/mainPageSlideData.js
+++ modles/mainPageSlideData.js
@@ -1,9 +1,9 @@
 const mainPageSlideData = [
-    { pic: 'http://localhost:8000/public/mainPageSlidePics/15380434541828.jpeg', href: '/#/second', description: 'this is a test' },
-    { pic: 'http://localhost:8000/public/mainPageSlidePics/15381044615697.jpeg', href: '/#/second', description: 'this is a test' },
-    { pic: 'http://localhost:8000/public/mainPageSlidePics/15381045921996.jpeg', href: '/#/second', description: 'this is a test' },
-    { pic: 'http://localhost:8000/public/mainPageSlidePics/15381046688621.jpeg', href: '/#/second', description: 'this is a test' },
-    { pic: 'http://localhost:8000/public/mainPageSlidePics/15381048978867.jpeg', href: '/#/second', description: 'this is a test' },
+    { pic: 'http://localhost:8000/public/mainPageSlidePics/15380434541828.jpeg', href: '#', description: 'this is a test' },
+    { pic: 'http://localhost:8000/public/mainPageSlidePics/15381044615697.jpeg', href: '#', description: 'this is a test' },
+    { pic: 'http://localhost:8000/public/mainPageSlidePics/15381045921996.jpeg', href: '#', description: 'this is a test' },
+    { pic: 'http://localhost:8000/public/mainPageSlidePics/15381046688621.jpeg', href: '#', description: 'this is a test' },
+    { pic: 'http://localhost:8000/public/mainPageSlidePics/15381048978867.jpeg', href: '#', description: 'this is a test' },
 ]
 
 function queryMainPageSlideData(){
