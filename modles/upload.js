152487438b11dcd03cb766bb01c5c7fcd5aebd01
jasontianxie-sub <540022044@qq.com>
2019-11-11 11:14:26 +0800

    新增一个文件，仅用于测试

---
+++ modles/upload.js
@@ -0,0 +1,21 @@
+const instanse = require("./index");
+
+const User = instanse.sequelize.define("users", {
+    id: { type: instanse.Sequelize.INTEGER, primaryKey: true },
+    name: instanse.Sequelize.CHAR,
+    email: instanse.Sequelize.CHAR,
+    pass: instanse.Sequelize.CHAR,
+},{
+    timestamps: false
+});
+
+function queryUsers(name, pass) {
+    return User.findAll({
+        where: {
+            name:name,
+            pass:pass
+        }
+    });
+}
+
+module.exports = queryUsers;
