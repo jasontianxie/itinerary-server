30cf60b4f8c1dd93ff9c897726aba70cbf529427
jasontianxie-sub <540022044@qq.com>
2020-01-23 12:10:14 +0800

    eslint 格式化

--- mysql.js
+++ mysql.js
@@ -1,42 +1,42 @@
-const express = require('express');
-// const mysql = require('mysql');
+const express = require("express");
+// const mysql = require("mysql");
 const app = express();
 const port = 3333;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const router = require("./routers/index");
-const cors = require('cors');
+const cors = require("cors");
 
 // const connection = mysql.createConnection({
-//     host: 'localhost',
+//     host: "localhost",
 //     port: 3306,
-//     user: 'root',
-//     password: '123456ABCabc',
-//     database: 'itinerary'
+//     user: "root",
+//     password: "123456ABCabc",
+//     database: "itinerary"
 // });
 // connection.connect(function (err) {
 //     if (err) {
-//         console.error('error connecting: ' + err.stack);
+//         console.error("error connecting: " + err.stack);
 //         return;
 //     }
 
-//     console.log('connected as id ' + connection.threadId);
+//     console.log("connected as id " + connection.threadId);
 // });
 
 var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-  }
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204 
+};
 app.use(cors(corsOptions));
 
-app.use('/public',express.static('static',{index: false}));
-app.use('/public/address',(req,res,next) => {
-  res.end(JSON.stringify([]))
-})
-app.use(express.static('front-end'));
+app.use("/public",express.static("static",{index: false,}));
+app.use("/public/address",(req,res) => {
+    res.end(JSON.stringify([]));
+});
+app.use(express.static("front-end"));
 
 app.use(bodyParser.json()); // for parsing application/json
 
-app.use('/', router);
-app.use('/api/uploads', require('./routers/upload_parts'))
+app.use("/", router);
+app.use("/api/uploads", require("./routers/upload_parts"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routers/upload_parts.js
+++ routers/upload_parts.js
@@ -1,74 +1,74 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer  = require('multer');
-const path = require('path');
-const fs = require('fs');
-const rimraf = require('../utils/removeDir');
-const upload = multer({ dest: 'uploads/multer/' });
+const multer  = require("multer");
+const path = require("path");
+const fs = require("fs");
+const rimraf = require("../utils/removeDir");
+const upload = multer({ dest: "uploads/multer/",});
 
-router.post('/parts', upload.single('file'),  (req, res) => {
-  // 接受图片唯一标识符号
+router.post("/parts", upload.single("file"),  (req, res) => {
+    // 接受图片唯一标识符号
     let fileId = req.body.fileId;
     // 接受切片索引
     let pieceNumber = req.body.pieceNumber;
     // 建立图片存储目录
-    let filepath = path.join(__dirname,'..','uploads/named_ordered_parts',fileId);
+    let filepath = path.join(__dirname,"..","uploads/named_ordered_parts",fileId);
     // 判断目录是否存在，存在的话直接使用并存储切片，不存在的话就新建。
     if (fs.existsSync(filepath)) {
-      fs.readFile(req.file.path, function (err, data) { //读取请求中multer处理过的文件数据，并按照uuid和分片的序号为分片的名字写入到服务器上，这里的req.file.path就是最开始multer初始化的时候定义的文件存储位置
-        fs.writeFile(path.join(filepath, pieceNumber), data, (err) => {
-          if (!err) {
-            fs.unlink(req.file.path, (err) => { //删除multer生成的临时文件
-              if (err) throw err;
-              console.log('临时文件已删除');
+        fs.readFile(req.file.path, function (err, data) { //读取请求中multer处理过的文件数据，并按照uuid和分片的序号为分片的名字写入到服务器上，这里的req.file.path就是最开始multer初始化的时候定义的文件存储位置
+            fs.writeFile(path.join(filepath, pieceNumber), data, (err) => {
+                if (!err) {
+                    fs.unlink(req.file.path, (err) => { //删除multer生成的临时文件
+                        if (err) throw err;
+                        console.log("临时文件已删除");
+                    });
+                    res.send("写入后面的文件");
+                }
             });
-            res.send("写入后面的文件")
-          }
-        })
-      })
+        });
     } else {
-      fs.mkdirSync(filepath);
-      fs.readFile(req.file.path, function (err, data) {
-        fs.writeFile(path.join(filepath, pieceNumber), data, (err) => {
-          if (!err) {
-            fs.unlink(req.file.path, (err) => { //删除multer生成的临时文件
-              if (err) throw err;
-              console.log('临时文件已删除');
+        fs.mkdirSync(filepath);
+        fs.readFile(req.file.path, function (err, data) {
+            fs.writeFile(path.join(filepath, pieceNumber), data, (err) => {
+                if (!err) {
+                    fs.unlink(req.file.path, (err) => { //删除multer生成的临时文件
+                        if (err) throw err;
+                        console.log("临时文件已删除");
+                    });
+                    res.send("第一次写入并新建文件夹");
+                }
             });
-            res.send("第一次写入并新建文件夹")
-          }
-        })
-      })
+        });
     }
-})
+});
 
-router.post('/merge',  (req, res) => {
-  let {fileId, fileName} = req.body;
-  let fileSplit = fileName.split(".");
-  let fileExt = fileSplit[fileSplit.length - 1];
-  let partsPath = path.resolve(process.cwd(), `uploads/named_ordered_parts/${fileId}`);
-  let  writeStream = fs.createWriteStream(`${partsPath}.${fileExt}`, {flags: 'a' });
-  fs.readdir(partsPath, (err, files) => {
-    let piece, piecesNumber = 1;
-    function merge() {
-      if (piecesNumber <= files.length) {
-        piece = fs.createReadStream(`${partsPath}/${piecesNumber}`)
-        piece.pipe(writeStream, { end: (piecesNumber === files.length) })
-        piece.on("end", () => {
-          piecesNumber++;
-          merge()
-        })
-      } else {
-        rimraf(partsPath);
-        res.send("上传完成，合并成功")
-      }
-    }
-    if (err) {
-      console.log("readdir出错")
-    } else {
-      merge()
-    }
-  })
-})
+router.post("/merge",  (req, res) => {
+    let {fileId, fileName,} = req.body;
+    let fileSplit = fileName.split(".");
+    let fileExt = fileSplit[fileSplit.length - 1];
+    let partsPath = path.resolve(process.cwd(), `uploads/named_ordered_parts/${fileId}`);
+    let  writeStream = fs.createWriteStream(`${partsPath}.${fileExt}`, {flags: "a",});
+    fs.readdir(partsPath, (err, files) => {
+        let piece, piecesNumber = 1;
+        function merge() {
+            if (piecesNumber <= files.length) {
+                piece = fs.createReadStream(`${partsPath}/${piecesNumber}`);
+                piece.pipe(writeStream, { end: (piecesNumber === files.length),});
+                piece.on("end", () => {
+                    piecesNumber++;
+                    merge();
+                });
+            } else {
+                rimraf(partsPath);
+                res.send("上传完成，合并成功");
+            }
+        }
+        if (err) {
+            console.log("readdir出错");
+        } else {
+            merge();
+        }
+    });
+});
 
-module.exports = router
+module.exports = router;
