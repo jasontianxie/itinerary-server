ff455182f5bd7d87edf20692b5a1377ba9d89d2c
jasontianxie-sub <540022044@qq.com>
2020-02-28 19:40:04 +0800

    游记id必须存在

--- routers/spots.js
+++ routers/spots.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {createSpot} = require("../models/spots")
+const {createSpot,} = require("../models/spots");
 
 router.post("/update", (req, res) => {
     const data = req.body;
     createSpot({
-        itineraryId: data.itineraryId || 0,
+        itineraryId: data.itineraryId,
         description: data.spotDescription || '',
         level1: data.level1 ? ((data.level1 &&　typeof(data.level1) === 'string') ? data.level1 : data.level1.name) : '',
         level2: data.level2 ? ((data.level2 &&　typeof(data.level2) === 'string') ? data.level2 : data.level2.name) : '',
@@ -19,7 +19,7 @@ router.post("/update", (req, res) => {
         latitude: data.latitude || '',
         time: data.time || 0
     }).then((result) => {
-        res.send({code: 0, spotId: result.spotId, message: '写入地点信息成功'})
+        res.send({code: 0, spotId: result.spotId, spotId: result.spotId, message: '写入地点信息成功'})
     }).catch((err) => {
         res.send({code:1, message: '写入地点信息失败'})
     })
