7f7ecb28dffb6bbbb59b82215465df651f8198ae
jasontianxie-sub <540022044@qq.com>
2020-03-17 19:15:10 +0800

    refs IS-11 更新登陆功能

---
+++ routers/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const {queryUsers,} = require("../models/users");
+
+router.post("/login",(req,res) => {
+    queryUsers(req.body.name, req.body.password).then((results) =>{
+        res.send(results);
+    },(errors) =>{
+        console.log(errors);
+        res.end();
+    });
+});
