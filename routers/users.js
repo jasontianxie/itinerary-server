6ffc725bd3a6929f9f3ebdb714d8c174866af806
jasontianxie <1986tianxie@sina.com>
2020-03-19 00:46:17 +0800

    用户是唯一的

--- routers/users.js
+++ routers/users.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const {queryUsers,} = require("../models/users");
 
 router.post("/login",(req,res) => {
-    queryUsers(req.body.name, req.body.password).then((results) =>{
-        res.send(results);
+    queryUsers(req.body.username, req.body.password).then((results) =>{
+        res.send(results[0]);
     },(errors) =>{
         console.log(errors);
         res.end();
