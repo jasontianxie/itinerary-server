daa92f86cd53262569925cdf58c6b1c261df8d9a
jasontianxie-sub <540022044@qq.com>
2020-03-26 12:01:25 +0800

    refs IS-12 更新返回的数据格式

--- routers/itineraries.js
+++ routers/itineraries.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {findItineraries,} = require("../models/itineraries");
+// const {findItineraries,} = require("../models/itineraries");
 const {findItinerariesUseUserId,} = require("../models/userMapItineraries");
 
 router.get("/list",(req,res) => {
-    findItinerariesUseUserId({userId: req.query.userid}).then((results) =>{
-        res.send({data:results});
+    findItinerariesUseUserId({userId: req.query.userid,}).then((results) =>{
+        res.send(results);
     }).catch(() => {
-        res.end()
+        res.end();
     });
 });
 
