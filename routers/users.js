276fbb639b969c79495973632a501c8f59b38b1c
jasontianxie <1986tianxie@sina.com>
2021-01-31 12:09:53 +0800

    用户注册接口

--- routers/users.js
+++ routers/users.js
@@ -45,4 +45,23 @@ router.post("/login",(req,res) => {
     });
 });
 
+router.post("/signup",(req,res) => {
+  // needLogin(req, res);
+  queryUsers(req.body.username, req.body.password).then((results) =>{
+      
+      req.session.regenerate(function(err) {
+          if(err){
+            return res.json({ret_code: 2, ret_msg: '登录失败'});        
+          }
+           
+          req.session.username = results[0].name;
+          req.session.password = results[0].pass;
+          res.send(results[0]);              
+        });
+  },(errors) =>{
+      console.log(errors);
+      res.end();
+  });
+});
+
 module.exports = router;
